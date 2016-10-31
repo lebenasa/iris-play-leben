d42addf1964f5bd9bec46e209605fb55cdd3be92
Leben Asa <lebenasa@hotmail.com>
2016-10-31 19:58:14 +0700

    Initialized repository for training with Iris framework

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# Playing with Iris Framework
+
+I need to get wet and dirty with web development in Go, so let's start a simple website using Iris.
+
+## Goal
+    1. A landing page where user can register or login
+    2. Once logged in, user can create a blog post using raw HTML (later: Markdown)
+
+
+
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "github.com/kataras/iris"
+)
+
+type baseTemplate struct {
+    Title, Resources string
+}
+
+type user struct {
+    Name, Email string
+    Password string // hehehe
+}
+
+// App provides necessary data and functions to handle requests
+type App struct {
+    RegisteredUsers []user
+}
+
+// Static information about the site
+const (
+    ResourceURL string = "/res"
+    Port string = ":8080"
+)
+
+func main() {
+    app := App{}
+    iris.OptionIsDevelopment(true)
+    iris.Static(ResourceURL, "./templates/", 1)
+
+    iris.Get("/", app.home)
+
+    app.RegisteredUsers = append(app.RegisteredUsers, user{ "admin", "admin@blogexample.com", "1234!"})
+    app.RegisteredUsers = append(app.RegisteredUsers, user{ "lebenasa", "lebenasa@hotmail.com", "1234" })
+
+    iris.Get("/join-check/username/:name", app.checkUsername)
+
+    iris.Listen(Port)
+}
+
+func (a App) home(ctx *iris.Context) {
+    ctx.Render("index_template.html", baseTemplate{"Iris Play!", ResourceURL}, iris.RenderOptions{"gzip": true})
+}
+
+func (a App) checkUsername(ctx *iris.Context) {
+    testname := ctx.Param("name")
+    for _, i := range a.RegisteredUsers {
+        if testname == i.Name {
+            ctx.JSON(iris.StatusOK, map[string]string{"available": "true"})
+            return
+        }
+    }
+    ctx.JSON(iris.StatusOK, map[string]string{"available":"false"})
+}
